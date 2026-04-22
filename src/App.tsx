2ddf27e2e688da5b9df92840c6acea2b925e7d05
Serhii Shimko <shimkosergij2010@gmail.com>
2026-04-22 22:19:04 +0300

    completed the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,151 @@
 import './App.scss';
+import { TodoList } from './components/TodoList';
+import { useState } from 'react';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { title } from 'process';
+import { Todo } from './type/Todo';
+import { User } from './type/User';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+const ENGLISH_SUMBOLS = 'abcdefhijgklmnopqrstuvwxyz';
+const UKRAINIAN_SUMBOLS = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя';
+const NUMBER = ' 0123456789';
+
+const todosWithUser = todosFromServer.map(todo => {
+  const userNow: User = usersFromServer.find((user: User) => {
+    if (user.id === todo.userId) {
+      return true;
+    }
+
+    return false;
+  })!;
+
+  return {
+    ...todo,
+    user: userNow,
+  };
+});
 
 export const App = () => {
+  const [todos, setTodos] = useState(todosWithUser);
+  const users: User[] = usersFromServer;
+
+  const [newUser, setNewUser] = useState(0);
+  const [newTitle, setNewTitle] = useState('');
+
+  const [submit, setSubmit] = useState(false);
+
+  const clearForm = () => {
+    setNewUser(0);
+    setNewTitle('');
+  };
+
+  const validationTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const word = String(e.target.value);
+    let valid: boolean = true;
+
+    word.split('').map(w => {
+      if (
+        !NUMBER.includes(w) &&
+        !UKRAINIAN_SUMBOLS.includes(w) &&
+        !ENGLISH_SUMBOLS.includes(w) &&
+        !UKRAINIAN_SUMBOLS.toUpperCase().includes(w) &&
+        !ENGLISH_SUMBOLS.toUpperCase().includes(w)
+      ) {
+        valid = false;
+      }
+    });
+
+    if (valid) {
+      setNewTitle(e.target.value);
+    } else {
+      return;
+    }
+  };
+
+  const validationForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmit(true);
+
+    if (newUser !== 0 && newTitle !== '') {
+      const maxId = Math.max(0, ...todos.map((todo: Todo) => +todo.id));
+      const userNow: User = usersFromServer.find((user: User) => {
+        if (user.id === newUser) {
+          return true;
+        }
+
+        return false;
+      })!;
+
+      console.log(maxId + 1);
+
+      const newTodo: Todo = {
+        id: maxId + 1,
+        title: newTitle,
+        completed: false,
+        userId: newUser,
+        user: userNow,
+      };
+
+      setTodos(prev => [...prev, newTodo]);
+      setSubmit(false);
+      clearForm();
+    }
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form
+        action="/api/todos"
+        method="POST"
+        onSubmit={e => {
+          validationForm(e);
+        }}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <label htmlFor="title">Enter a title:</label>
+          <input
+            type="text"
+            id="title"
+            placeholder="title..."
+            data-cy="titleInput"
+            value={newTitle}
+            onChange={e => {
+              validationTitle(e);
+            }}
+          />
+          {submit && !newTitle && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <label htmlFor="select">Choose a user:</label>
+          <select
+            data-cy="userSelect"
+            value={newUser}
+            id="select"
+            onChange={e => {
+              setNewUser(+e.target.value);
+            }}
+          >
             <option value="0" disabled>
               Choose a user
             </option>
+            {users.map(user => {
+              return (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              );
+            })}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {submit && !newUser && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -29,33 +153,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,22 @@
-export const TodoInfo = () => {};
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../type/Todo';
+import classNames from 'classnames';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed === true,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo key={todo.id} user={todo.user} />;
+    </article>
+  );
+};
